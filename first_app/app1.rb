6feae53537f9effaf94c7d6b741c65eaa15ff0c7
PinkDeer <bunman@mail.ru>
2017-05-21 11:52:34 +0300

    Update First_App

--- first_app/app1.rb
+++ first_app/app1.rb
@@ -4,13 +4,35 @@ get '/' do
   erb :index
 end
 
+get '/contacts' do
+  @title = 'Contacts'
+  @message = 'Phone number: 12313123'
+  erb :message
+end
+
+get '/faq' do
+  @title = 'FAQ'
+  @message = 'Under construction'
+  erb :message
+end
+
+get '/something' do
+  @title = 'something'
+  @message = 'Bla bla'
+  erb :message
+end
+
 post '/' do
-  @login = params[:aaa]
-  @password = params[:bbb]
+  @login = params[:login]
+  @password = params[:password]
 
   if @login == 'admin' && @password == 'secret'
     erb :welcome
+  elsif @login == 'admin' && @password == 'admin'
+    @message = 'Haha, nice try! Access denied!'
   else
+    @message = 'Access denied!'
     erb :index
-end
+  end
+
 end
