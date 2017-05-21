56d7c4544872f89823c5a466b07202e811652215
PinkDeer <bunman@mail.ru>
2017-05-22 00:36:14 +0300

    Update Barbershop

--- barbershop_bootstrap/app.rb
+++ barbershop_bootstrap/app.rb
@@ -11,3 +11,14 @@ end
 get '/visit' do
   erb :visit
 end
+
+post '/visit' do
+
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+  @color = params[:color]
+
+  erb "Ok, #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+end
